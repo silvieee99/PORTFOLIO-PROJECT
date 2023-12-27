726868c9f538cf144474f0a63f9f0ccae887135d
Silvia Uzie <146796592+silvieee99@users.noreply.github.com>
2023-12-27 19:03:51 +0100

    Update OLYMPIC Q N  A PROJECT (1).sql

--- OLYMPIC Q N  A PROJECT (1).sql
+++ OLYMPIC Q N  A PROJECT (1).sql
@@ -1,71 +1,73 @@
 --ANALYSIS OF 120 YEARS OF OLYMPIC DATA
 
-select*
-from OlympicHistory
-select*
-from OlympicRegions
+SELECT*
+FROM OlympicHistory
+SELECT*
+FROM OlympicRegions
 
 --How many olympics games have been held?
-select count( distinct Games) as No_Of_Olympic_Games
-from OlympicHistory
+SELECT COUNT( DISTINCT Games) AS No_Of_Olympic_Games
+FROM OlympicHistory
 
 --List down all Olympics games held so far
-select distinct Games,City
-from OlympicHistory
-order by Games desc
+SELECT DISTINCT Games,City
+FROM OlympicHistory
+ORDER BY Games DESC
 
 --Mention the total no of nations who participated in each olympics game?
 
-select count(distinct reg.region),Games
-from OlympicHistory his
- join OlympicRegions reg
-	on his.NOC = reg.NOC
+SELECT COUNT(DISTINCT reg.region),Games
+FROM OlympicHistory his
+ JOIN OlympicRegions reg
+	ON his.NOC = reg.NOC
 	--where Games = '1896 summer'
-	group by Games
-	order by Games
+	GROUP BY Games
+	ORDER BY Games
 
 
 --Which year saw the highest and lowest no of countries participating in olympics?
 --HIGHEST NO OF COUNTRY
-select top 1 count(distinct reg.region),Games
-from OlympicHistory his
- join OlympicRegions reg
-	on his.NOC = reg.NOC
+SELECT top 1 COUNT(DISTINCT reg.region),Games
+FROM OlympicHistory his
+ JOIN OlympicRegions reg
+	ON his.NOC = reg.NOC
 	--where Games = '1896 summer'
-	group by Games
-	order by Games desc 
+	GROUP BY Games
+	ORDER BY Games DESC 
 
 --LOWEST NO OF COUNTRY
-select top 1 count(distinct reg.region),Games
-from OlympicHistory his
- join OlympicRegions reg
-	on his.NOC = reg.NOC
+SELECT top 1 COUNT(DISTINCT reg.region),Games
+FROM OlympicHistory his
+ JOIN OlympicRegions reg
+	ON his.NOC = reg.NOC
 	--where Games = '1896 summer'
-	group by Games
-	order by Games asc
+	GROUP BY Games
+	ORDER BY Games ASC
 
---Which nation has participated in all of the olympic games?
-select  reg.region,count( distinct Games) as TotalNoOfParticipatedGames 
-from OlympicHistory his
-join OlympicRegions reg
-	 on his.NOC = reg.NOC
-group by reg.region
-having count(distinct Games ) = (select count(distinct Games) from OlympicHistory)
+--WHICH NATION HAS PARTICIPATED IN ALL OF THE OLYMPIC GAMES?
+SELECT  reg.region,COUNT( DISTINCT Games) AS TotalNoOfParticipatedGames 
+FROM OlympicHistory his
+JOIN OlympicRegions reg
+	 ON his.NOC = reg.NOC
+GROUP BY reg.region
+HAVING COUNT(DISTINCT Games ) = (SELECT COUNT(DISTINCT Games) FROM OlympicHistory)
 
 
 
---Identify the sport which was played in all summer olympics
-select Sport, count(distinct Games) as Total_Games
-from OlympicHistory
-where Season = 'summer'
-group by Sport
-having count(distinct Games ) = (select count(distinct Games) from OlympicHistory where Season = 'summer')
+--IDENTIFY THE SPORTS THAT WAS PLAYED IN ALL OF SUMMER OLYMPICS
+SELECT Sport, COUNT(DISTINCT Games) AS Total_Games
+FROM OlympicHistory
+WHERE Season = 'summer'
+GROUP BY Sport
+HAVING COUNT(DISTINCT Games ) = (SELECT COUNT(DISTINCT Games) FROM OlympicHistory WHERE Season = 'summer')
 
---Which Sports were just played only once in the olympics?
-select Sport, MIN(Games) as Year_Played
-from OlympicHistory
-group by Sport
-having count(distinct Games) = 1
+--WHICH SPORTS WERE PLAYED JUST ONCE IN THE OLYMPICS
+SELECT Sport, MIN(Games) as Year_Played
+FROM OlympicHistory
+GROUP BY Sport
+HAVING COUNT(DISTINCT Games) = 1
+
+
 
 
 
