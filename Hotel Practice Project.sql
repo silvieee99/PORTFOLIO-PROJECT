6a2fb62e6b250acde03861f44bbd907eb1235560
Silvia Uzie <146796592+silvieee99@users.noreply.github.com>
2023-12-27 18:45:44 +0100

    Update Hotel Practice Project.sql

--- Hotel Practice Project.sql
+++ Hotel Practice Project.sql
@@ -1,43 +1,43 @@
---select*
---from Y2018
---union
---select*
---from Y2019
---union
---select*
---from Y2020
+SELECT*
+FROM Y2018
+UNION
+SELECT*
+FROM Y2019
+UNION
+SELECT*
+FROM Y2020
 
 --IS OUR HOTEL REVENUE GROWING YEARLY By hotel type ?
 --ADR is daily rate
 
-with Hotels as (
-select* from Y2018
+WITH Hotels AS (
+SELECT* FROM Y2018
+UNION
+SELECT* FROM Y2019
 union
-select* from Y2019
-union
-select* from Y2020
+SELECT* FROM Y2020
 )
-select arrival_date_year, hotel ,round(sum((stays_in_weekend_nights + stays_in_week_nights)* adr),2 )as Revenue 
-from Hotels
-group by arrival_date_year,hotel
-order by arrival_date_year
+SELECT arrival_date_year, hotel ,round(sum((stays_in_weekend_nights + stays_in_week_nights)* adr),2 )as Revenue 
+FROM Hotels
+GROUP BY arrival_date_year,hotel
+ORDER BY arrival_date_year
 
 --JOIN WITH THE MARKET SEGMENT TABLE
 
-with Hotels as (
-select* from Y2018
-union
-select* from Y2019
-union
-select* from Y2020
+WITH Hotels AS (
+SELECT* FROM Y2018
+UNION
+SELECT* FROM Y2019
+UNION
+SELECT* FROM Y2020
 )
-select*
-from Hotels hot
-left join MarketSegment mark
-	on hot.market_segment = mark.market_segment
-left join MealCost mea
-	on Mea.meal = hot.meal
+SELECT*
+FROM Hotels hot
+LEFT JOIN MarketSegment mark
+	ON hot.market_segment = mark.market_segment
+LEFT JOIN MealCost mea
+	ON Mea.meal = hot.meal
 
-	select*
-	from MarketSegment
+	SELECT*
+	FROM MarketSegment
 
